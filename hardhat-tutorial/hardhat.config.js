091e6abfe5e77755854e939572ec740d85a2814f
Xhistr <vaibhavdwivedi.iitr@gmail.com>
2023-03-22 12:14:45 +0530

    Completed

---
+++ hardhat-tutorial/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
+
+//contract address:- 0x3e4681db89E293EdEBC64EA16C9b8836347fd205
